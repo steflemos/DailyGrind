b53c6c3649fd612e0d55feedd12f2be7db90a1b9
Eduardo Paixão Sehn <78269439+Eduardo668@users.noreply.github.com>
2022-06-10 15:32:08 -0300

    Adição da página Ranking

--- src/components/Ranking/index.js
+++ src/components/Ranking/index.js
@@ -1,25 +1,31 @@
 import React from "react";
-import {View, Text, StyleSheet} from 'react-native'
+import { View, Image,Text, TouchableOpacity } from "react-native";
+import styles from "../../styles/ranking";
+import {FontAwesome} from '@expo/vector-icons'
 
-export default function Ranking(){
+
+const Ranking = () => {
+    
     return(
         <View style={styles.container}>
-            <Text style={styles.text}>Ranking</Text>
+            <View style={styles.header}>
+                <Image style={styles.controleIndex} source={require('../../styles/assets/controle.png')} />
+                <Text style={styles.numerosHeader}>3/3</Text>
+                <Image style={styles.levels} source={require('../../styles/assets/levels.png')} />
+            </View>
+            <View style={styles.main}>
+                <Text style={styles.titulo}>Ranking</Text>
+                <View style={styles.containerLista}>
+                    <View style={styles.containerGrupoTitle}>
+                        <Text style={styles.grupoTitle}>Grupos</Text>
+                    </View>
+                    <View style={styles.grupoList}>
+                        <Text style={styles.textTarefa}>Grupo da Igreja</Text>
+                    </View>
+                </View>
+            </View>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
-        backgroundColor: '#6F8AB7'
-        
-    },
-
-    text:{
-        fontSize:25,
-        fontWeight:'bold'
-    }
-})
+export default Ranking
