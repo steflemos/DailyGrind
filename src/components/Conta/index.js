b4ac3e7d56b2ffd6006c0792d91280c93126b225
Eduardo Paixão Sehn <78269439+Eduardo668@users.noreply.github.com>
2022-06-28 17:46:19 -0300

    BRUNO POR FAVOR TERMINA ESSA DESGRAALOGO

--- src/components/Conta/index.js
+++ src/components/Conta/index.js
@@ -1,20 +1,42 @@
-import React, {useState} from "react";
-import { View,Text, TouchableOpacity, TextInput } from "react-native";
-import styles from "../../styles/new";
+import React from "react";
+import { View, Image,Text, TouchableOpacity } from "react-native";
+import styles from "../../styles/conta.js";
 import {FontAwesome} from '@expo/vector-icons'
 
-const Conta = () => {
+
+const Opcoes = ({navigation}) => {
     
     return(
         <View style={styles.container}>
+            <View style={styles.header}>
+                <Image style={styles.controleIndex} source={require('../../styles/assets/controle.png')} />
+                <Text style={styles.numerosHeader}>3/3</Text>
+                <Image style={styles.levels} source={require('../../styles/assets/levels.png')} />
+            </View>
             <View style={styles.main}>
-                <Text style={styles.titulo}>.</Text>
-                <View style={styles.containerLista}>
                
+                <View style={styles.containerLista}>
+                    
+                <TouchableOpacity onPress={() => navigation.navigate("Conta")}style={styles.nick}>
+                        <Text style={styles.textTarefa}>Conta</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => navigation.navigate("Notificacoes")}style={styles.email}>
+                        <Text style={styles.textTarefa}>Notificações</Text>
+                    </TouchableOpacity>
+                    
+                    <TouchableOpacity onPress={() => navigation.navigate("Ajuda")}style={styles.senha}>
+                        <Text style={styles.textTarefa}>Ajuda</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => navigation.navigate("Sobre")}style={styles.deletar}>
+                        <Text style={styles.textTarefa}>Sobre</Text>
+                    </TouchableOpacity>
+
                 </View>
             </View>
         </View>
     )
 }
 
-export default Conta;
+export default Opcoes;
