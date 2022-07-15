aa4c0e420f50a832cd872cd36189d12dfaac365e
Gustavo Silles <sillesgustavo05@gmail.com>
2022-07-15 17:43:19 -0300

    -a

--- src/components/pages/Cadastro/index.js
+++ src/components/pages/Cadastro/index.js
@@ -5,23 +5,67 @@ import styles from './styles'
 import api from '../../../api/index'
 
 
+export const dadosUsuario = []
 const Cadastro = ({navigation}) =>{
+  const [usuario, setUsuario] = useState([]);
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [passwordInput, setPasswordInput] = useState('');
   const [confirm, setconfirm] = useState('');
-  const [error, setError] = useState(null)
+  const [error, setError] = useState(null);
  
 
-  const validarCadastro = () =>{
-    if((name, email, passwordInput, confirm !== "")){
-      signIn({nome : name, email : email, password : passwordInput, confirmar : confirm })
-    }else{
-      setError(null)
-      setError('PREENCHA TODOS OS CAMPOS')
-      return
+  const getUsuarios = async () => {
+    try{
+        const response = await fetch('http://localhost:3000/usuario')
+        const data = response.json()
+        data.then(
+            (val) => setUsuario(val)
+        )
+    }catch(error){
+        console.log(error)
     }
+}
+  
+getUsuarios()
+
+const getUsuarioDados = () => {
+          
+  console.log(email, name)
+  if(email.length > 2 && email.includes('@',3) && email.includes('.',5)){
+       if(name.length > 3){
+            if(!name.includes(' ',0)){
+                 if(usuario.find((dados) => {return dados.email === email}) == undefined){
+                      if(usuario.find((dados) => {return name === dados.name}) == undefined){
+                           dadosUsuario.push({email: email, apelido: name})
+                      }else{
+                           console.log('usuario existe')
+                      }
+                 }else{
+                      console.log('email existe')
+                 }
+            }else{
+                 console.log('Nome incorreto')
+            }
+       }else{
+            console.log('nome curto')
+       }
+  }else{
+       console.log('email errado')
   }
+}
+
+  // const validarCadastro = () =>{
+  //   if((name, email, passwordInput, confirm !== "")){
+  //     signIn({nome : name, email : email, password : passwordInput, confirmar : confirm })
+  //   }else{
+  //     setError(null)
+  //     setError('PREENCHA TODOS OS CAMPOS')
+  //     return
+  //   }
+
+
+  // }
   
   
 
@@ -37,7 +81,7 @@ const Cadastro = ({navigation}) =>{
 
           </View>
             <TextInput 
-            onChangeText={setName}
+            onChangeText={name => setName(name)}
             value={name}
             label="Nome Completo"
             style={styles.input} 
@@ -47,7 +91,7 @@ const Cadastro = ({navigation}) =>{
             underlineColor="#615D6C"
             />
 
-            <TextInput  onChangeText={setEmail}
+            <TextInput  onChangeText={email => setEmail(email)}
             value={email}
             label="E-mail"
             placeholder='email@email.com'
@@ -61,7 +105,7 @@ const Cadastro = ({navigation}) =>{
 
             <TextInput 
             value={passwordInput}
-            onChangeText={setPasswordInput}
+            onChangeText={password => setPasswordInput(password)}
             label="Senha"
             left={<TextInput.Icon name="lock" color = "#615D6C"/>}
             style={styles.input}
@@ -86,7 +130,7 @@ const Cadastro = ({navigation}) =>{
             <Text style={styles.setError}>{error}</Text>
            
             {/* <TouchableOpacity onPress={() => salvar()} style={styles.btnLogin}><Text style={styles.textBtnLogin}>Cadastrar</Text></TouchableOpacity> */}
-            <TouchableOpacity onPress={validarCadastro} style={styles.btnLogin}><Text style={styles.textBtnLogin}>Cadastrar</Text></TouchableOpacity>
+            <TouchableOpacity onPress={() => getUsuarioDados} style={styles.btnLogin}><Text style={styles.textBtnLogin}>Cadastrar</Text></TouchableOpacity>
             <TouchableOpacity onPress={() => navigation.navigate('Login')}><Text style={styles.cadastrar}>Ja possuo uma conta</Text></TouchableOpacity>
             
         </View>
